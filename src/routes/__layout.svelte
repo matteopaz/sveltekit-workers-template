5baef9676290c9f3eed60ec0c6400f7621daf24a
Matteo Paz <86904497+matteopaz@users.noreply.github.com>
2021-10-03 23:41:28 -0700

    Added demo and fixed filepath

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import 'normalize.css';
-    import '/static/global.css';
+    import '/global.css';
 </script>
 
 <slot></slot>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
+  import Counter from '$lib/Counter.svelte'
   console.log("Hello World");
 </script>
 
 <h1>Hello Svelte</h1>
-
+<Counter />
 <style lang="postcss">
   :global(body) {
     background-color: #e5e5e5;
+    display: grid;
+    place-items: center;
+    min-height: 100vh;
   } 
   
   h1 {
       font-family: sans-serif;
-      margin: 1rem auto;
-      font-size: 5rem;
+      margin: 0 auto;
+      font-size: 6rem;
       letter-spacing: 0.25rem;
       color: #ffa500;
       width: max-content;
